17a6c509920b875fe681d13277ae326d36c51072
Sanket832949 <85669078+Sanket832949@users.noreply.github.com>
2021-07-30 17:53:05 +0530

    Update Demo.java

--- Basic/src/Demo/Demo.java
+++ Basic/src/Demo/Demo.java
@@ -7,6 +7,8 @@ public class Demo
 		System.out.println("Hello Cjc");
 		System.out.println("Hello Sanket");
 		System.out.println("Hello Sagar");
+		System.out.println("Hello Manoj");
+		System.out.prinln("Hello abc");
 		
 	}
 }
